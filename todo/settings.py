2f55a752ea3493058d76b8a5f45f23d11f766aca
Om Official <omstha71@gmail.com>
2025-07-30 19:31:58 +0545

    full flegged todo app

--- todo/settings.py
+++ todo/settings.py
@@ -40,10 +40,12 @@ INSTALLED_APPS = [
     'myapp',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -133,3 +135,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
 }
+
+CORS_ALLOW_CREDENTIALS = True
+
